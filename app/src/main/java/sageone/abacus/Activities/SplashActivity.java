2c2c4ca8ef7402881a8e4938c2c6f8662b4aaceb
Oliver Tomaske <oliver.tomaske@sage.de>
2016-03-01 11:25:09 +0100

    adds splash screen

---
+++ app/src/main/java/sageone/abacus/Activities/SplashActivity.java
@@ -0,0 +1,45 @@
+package sageone.abacus.Activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import sageone.abacus.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static int SPLASH_TIME_OUT = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        _timeOut();
+    }
+
+
+    /**
+     * Initializes time out depending
+     * redirect to home activity.
+     */
+    private void _timeOut()
+    {
+        new Handler().postDelayed(new Runnable() {
+
+            /*
+             * Showing splash screen with a timer. This will be useful when you
+             * want to show case your app logo / company
+             */
+
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashActivity.this, HelloActivity.class);
+                startActivity(i);
+
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+
+}
