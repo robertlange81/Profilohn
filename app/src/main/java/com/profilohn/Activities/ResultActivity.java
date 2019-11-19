8410009c0e65d0fa220e46be515fe45becc36cdf
Robert Lange <Lange@PROJ103.ub.uni-leipzig.de>
2019-11-19 20:06:07 +0100

    rate app link basic

--- app/src/main/java/com/profilohn/Activities/ResultActivity.java
+++ app/src/main/java/com/profilohn/Activities/ResultActivity.java
@@ -1,7 +1,9 @@
 package com.profilohn.Activities;
 
-import android.app.Activity;
+import android.content.ActivityNotFoundException;
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 
 import android.support.v4.app.Fragment;
@@ -126,8 +128,27 @@ public class ResultActivity extends AppCompatActivity {
         }
     }
 
+    public void rateApp()
+    {
+        Uri uri = Uri.parse("market://details?id=" + ResultActivity.this.getPackageName());
+        Intent goToMarket = new Intent(Intent.ACTION_VIEW, uri);
+        // To count with Play market backstack, After pressing back button,
+        // to taken back to our application, we need to add following flags to intent.
+        goToMarket.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY |
+                Intent.FLAG_ACTIVITY_NEW_DOCUMENT |
+                Intent.FLAG_ACTIVITY_MULTIPLE_TASK);
+        try {
+            startActivity(goToMarket);
+        } catch (ActivityNotFoundException e) {
+            startActivity(new Intent(Intent.ACTION_VIEW,
+                    Uri.parse("http://play.google.com/store/apps/details?id=" + ResultActivity.this.getPackageName())));
+        }
+    }
+
     @Override
     public void onBackPressed() {
+
+        this.rateApp();
 
         if(this.isTablet) {
             this.finish();
